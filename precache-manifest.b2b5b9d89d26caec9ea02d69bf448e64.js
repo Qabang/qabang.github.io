2ed138141872fcc026558dab153438582b840b96
Sandra Lindström <sandra.lindstrm@gmail.com>
2021-02-02 15:47:40 +0100

    first commit

---
+++ precache-manifest.b2b5b9d89d26caec9ea02d69bf448e64.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50490f80f98086ef615",
+    "url": "/css/about.f4f94428.css"
+  },
+  {
+    "revision": "56f4dff997844ed640f8",
+    "url": "/css/app.95bb6e38.css"
+  },
+  {
+    "revision": "a70806729dd96ebec3aa",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "4959349a025891b216b0",
+    "url": "/css/list.05058f15.css"
+  },
+  {
+    "revision": "a329a8ff59c689641a1daec38c0126e0",
+    "url": "/img/logo.a329a8ff.svg"
+  },
+  {
+    "revision": "b17ae7b4ad274e423d0b7de18cf3d4a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e50490f80f98086ef615",
+    "url": "/js/about.19b4344d.js"
+  },
+  {
+    "revision": "56f4dff997844ed640f8",
+    "url": "/js/app.1bfea1b8.js"
+  },
+  {
+    "revision": "a70806729dd96ebec3aa",
+    "url": "/js/chunk-vendors.c178a794.js"
+  },
+  {
+    "revision": "4959349a025891b216b0",
+    "url": "/js/list.5887b5f8.js"
+  },
+  {
+    "revision": "3719b414d9f6a0c4f575df9e2b35d207",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
